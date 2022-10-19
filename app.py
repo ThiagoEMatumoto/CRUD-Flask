d8b99029c6a33041fdc2002ce1d16935b1154fd2
ThiagoEMatumoto <thiagoeijimatumoto@gmail.com>
2022-10-19 09:03:15 -0300

    Adicionando requirements

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Sky1Thi@go@localhost/youtube'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/youtube'
 db = SQLAlchemy(app)
 
 
@@ -15,5 +15,11 @@ class Usuario(db.Model):
     email = db.Column(db.String(100))
 
 
+@app.route("/usuarios", methods=["GET"])
+def selecionar_usuarios():
+    usuarios_classes = Usuario.query.all()
+    print(usuarios_classes)
+    return Response()
 
 
+app.run()
